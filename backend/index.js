d64af487886c6d7e19ef06847a71b8506c18a2c2
ridaapp250-cmyk <ridaapp250@gmail.com>
2026-02-26 17:20:22 +0200

    Added a frontend Cors route

--- backend/index.js
+++ backend/index.js
@@ -22,11 +22,11 @@ connectDB();
 // CORS - Allow your frontend to connect
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? [
-        'https://rida-production-d2a6.up.railway.app', 
+    ? [ 'https://rida.up.railway.app',
+        
         'https://www.ridaapp.com',
         'https://ridaapp.com',
-        'https://rida-1.onrender.com'
+       
       ]
     : [process.env.FRONTEND_URL || 'http://localhost:3000'],
   credentials: true
