1a1656bbeb327229669b44592774d0a09df7ee3f
ridaapp250-cmyk <ridaapp250@gmail.com>
2026-02-27 16:14:10 +0200

    Tel in nav changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1222,7 +1222,7 @@ const App = () => {
               <div className="col-md-3">
                 <h5>Contact Us</h5>
                 <p><i className="bi bi-envelope me-2"></i> helpline@ridaapp.com</p>
-                <p><i className="bi bi-telephone me-2"></i> +(250) 789543687</p>
+                <p><i className="bi bi-telephone me-2"></i> +(256) 770 493458</p>
               </div>
             </div>
             <hr className="bg-white bg-opacity-25" />
