75b051e49480a4caeb717695690dc9242e98afda
ridaapp250-cmyk <ridaapp250@gmail.com>
2026-02-28 13:28:57 +0200

    auth const added

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db.js');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
+const auth = require('./middleware/auth'); 
 
 console.log('🔍 Starting backend service...');
 
